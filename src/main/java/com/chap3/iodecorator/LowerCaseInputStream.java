483967f9d21454f01f6e88a860cb65873a4d01da
luckystar <stevezlh@hotmail.com>
2022-06-14 23:17:39 +0800

    add: io decorator

---
+++ src/main/java/com/chap3/iodecorator/LowerCaseInputStream.java
@@ -0,0 +1,48 @@
+/**
+ * @(#) LowerCaseInputStream.java 1.0 2022-06-14
+ * Copyright (c) 2022, AllNightBlues. ALL right reserved.
+ * AllNightBlues PROPRIETARY/CONFIDENTIAL. Use is subject to license terms.
+ */
+package com.chap3.iodecorator;
+
+import java.io.FilterInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
+/**
+ * @ClassName LowerCaseInputStream
+ * @description:
+ * @AUTHOR AllNightBlues
+ * @Date 2022/06/14 22:24
+ * @Version 1.0
+ **/
+public class LowerCaseInputStream extends FilterInputStream {
+
+    /**
+     * Creates a <code>FilterInputStream</code>
+     * by assigning the  argument <code>in</code>
+     * to the field <code>this.in</code> so as
+     * to remember it for later use.
+     *
+     * @param in the underlying input stream, or <code>null</code> if
+     *           this instance is to be created without an underlying stream.
+     */
+    protected LowerCaseInputStream(InputStream in) {
+        super(in);
+    }
+
+    @Override
+    public int read() throws IOException {
+        int c = in.read();
+        return c == -1 ? c : Character.toLowerCase((char) c);
+    }
+
+    @Override
+    public int read(byte[] b, int off, int len) throws IOException {
+        int result = in.read(b, off, len);
+        for (int i = off; i < off + result; i++) {
+            b[i] = (byte) Character.toLowerCase((char) b[i]);
+        }
+        return result;
+    }
+}
