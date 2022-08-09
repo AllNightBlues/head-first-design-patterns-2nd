90a0f6f549108e179b7e1e5829e320640681e0cc
luckystar <stevezlh@hotmail.com>
2022-08-09 21:10:13 +0800

    add: add cafemenu

--- src/main/java/com/chap9/menu/MenuTestDrive.java
+++ src/main/java/com/chap9/menu/MenuTestDrive.java
@@ -6,7 +6,8 @@ public class MenuTestDrive {
 	public static void main(String args[]) {
         Menu pancakeHouseMenu = new PancakeHouseMenu();
         Menu dinerMenu = new DinerMenu();
- 
+		Menu cafeMenu = new CafeMenu();
+
 		Waitress waitress = new Waitress(pancakeHouseMenu, dinerMenu);
 		
 		// With iterators
